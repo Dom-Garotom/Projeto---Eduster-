820c60aea6de22f892312c0bcf89b8b719b53d4b
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-05-09 16:27:42 -0300

    Criando páginas de login e criação de contas como também lincamos as páginas

---
+++ src/scripts/login_registro.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email').value;
+const senha = document.getElementById('senha').value;
+const url = '../aplicação.html'
+
+form.addEventListener('submit', ( form) =>{
+    form.preventDefault();
+
+    if (email === undefined || senha === undefined){
+        alert('Prencha os campos de forma correta!')
+    }
+
+      teste(url)
+})
+
+var teste = (url)=>{
+    window.location.href = url;
+}
