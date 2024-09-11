87bbea0055b244cb5a72984e260ba4c096919c3b
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-11 10:58:55 -0300

    simplifying code and creating dialog logic

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,54 +1,75 @@
 // Scripts dos cards da faq
 
-const cards_faq = document.querySelectorAll('.card_faq');
+const cards_faq = document.querySelectorAll(".card_faq");
 
-cards_faq.forEach(card =>{
-    card.addEventListener('click', () =>{
-
-        cards_faq.forEach( item =>{
-            const resposta = item.querySelector('.resposta_faq');
-            resposta.classList.remove('active');
-        });
-
-        const resposta = card.querySelector('.resposta_faq');
-        resposta.classList.toggle('active');
+cards_faq.forEach((card) => {
+  card.addEventListener("click", () => {
+    cards_faq.forEach((item) => {
+      const resposta = item.querySelector(".resposta_faq");
+      resposta.classList.remove("active");
     });
-});
 
+    const resposta = card.querySelector(".resposta_faq");
+    resposta.classList.toggle("active");
+  });
+});
 
 //  Scripts dos modais
 
-// const buttonDeAgendar = document.getElementById("buttonDeAgendar");
-// const modalDeAgendar = document.getElementById("modal");
-// const buttonDeSair = document.getElementById("sair");
+function showCreateSchedule() {
+  const modal = document.getElementById("modal-1");
+  const openButton = document.querySelector("li[variant-2]");
+  const closeButton = modal.querySelector(".button-close");
+  const agendarButton = modal.querySelector(".btn-dialog");
+  openButton.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    modal.close();
+  });
+
+  agendarButton.addEventListener("click", () => {
+    modal.close();
+  });
+}
+
+function showListSchedule() {
+  const modal = document.getElementById("modal-2");
+  const openButton = document.querySelector("li[variant-3]");
+  const closeButton = modal.querySelector(".button-close");
+  const agendarButton = modal.querySelector(".btn-dialog");
+  openButton.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    modal.close();
+  });
+
+  agendarButton.addEventListener("click", () => {
+    modal.close();
+  });
+}
+
+// script do menu
 
 
-// buttonDeAgendar.addEventListener('click', ()=>{
-//     modalDeAgendar.showModal()
-// })
-
-// buttonDeSair.addEventListener('click', () => {
-//     modalDeAgendar.close();
-// })
-
-
-// script do menu 
-
-
-let menuIcon = document.querySelector(".menu");
-let headerItems = document.querySelectorAll("#header-list li")
-let headerList = document.getElementById("header-list")
-let xMark = document.querySelector(".x-mark")
-
-menuIcon.addEventListener('click',()=>{
+function showMenuHeader() {
+  let menuIcon = document.querySelector(".menu");
+  let headerItems = document.querySelectorAll("#header-list li");
+  let headerList = document.getElementById("header-list");
+  let xMark = document.querySelector(".x-mark");
+  menuIcon.addEventListener("click", () => {
     headerList.style.right = 0;
-})
-xMark.addEventListener("click", () =>{
+  });
+  xMark.addEventListener("click", () => {
     headerList.style.right = "-100%";
-})
+  });
 
-headerItems.forEach( element =>{
-    element.addEventListener("click" , () =>{
-        headerList.style.right = "-100%";
-    })
-})
+  headerItems.forEach((element) => {
+    element.addEventListener("click", () => {
+      headerList.style.right = "-100%";
+    });
+  });
+}
