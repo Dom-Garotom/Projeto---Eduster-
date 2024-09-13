5009bead3ccf0f4140744dca13676f29a84955e5
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-13 15:54:44 -0300

    creating dialog of  list  of schedule, last create was dialog of reminder

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -35,8 +35,23 @@ function showCreateSchedule() {
 }
 
 function showListSchedule() {
-  const modal = document.getElementById("modal-3");
+  const modal = document.getElementById("modal-2");
   const openButton = document.querySelector("li[variant-3]");
+  const closeButton = modal.querySelector(".button-close");
+  const agendarButton = modal.querySelector(".btn-dialog");
+  openButton.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    modal.close();
+  });
+
+}
+
+function showReminder() {
+  const modal = document.getElementById("modal-3");
+  const openButton = document.querySelector("li[variant-4]");
   const closeButton = modal.querySelector(".button-close");
   const agendarButton = modal.querySelector(".btn-dialog");
   openButton.addEventListener("click", () => {
